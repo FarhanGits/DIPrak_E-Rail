3d0d2cb96ab0265408b975aae98bb375ec56d973
FarhanGits <daffafarhan05@gmail.com>
2023-11-26 14:11:14 +0700

    commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{ asset('style/styleHomePage.css') }}" />
+    <title>Homepage</title>
+</head>
+
+<body>
+    <div class="main">
+        <div class="leftSection">
+            <div class="logo">
+                <a href="">eRail</a>
+                <a href="">Access</a>
+            </div>
+
+            <div class="SubleftSection">
+                <div class="helloTraveller">Hello Travellers</div>
+                <div class="title">
+                    NIKMATI KENYAMANAN <br />
+                    DISETIAP PERJALANANMU!
+                </div>
+                <div class="subtitle">Train booking is a process of choosing and purchasing train seats online. It is an
+                    easy process but were are here to make it much better & simple.</div>
+                <div class="button">
+                    <a class="bigButton" href="">
+                        <div>TEMUKAN PERJALANANMU</div>
+                    </a>
+                    <a class="smallButton" href="">
+                        <img src="Vector.png" alt="" />
+                    </a>
+                </div>
+
+                <div class="option">
+                    <a href="">
+                        <div class="option-1">Mari Rencanakan Liburanmu! ></div>
+                    </a>
+
+                    <a href="">
+                        <div class="option-2">Lihat Paket Liburan Disini! ></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="rightSection"></div>
+    </div>
+
+    <div class="footer">
+
+        <div class="row1">
+            <div class="logoFooter">
+                <a href="">eRail</a>
+                <a href="">Access</a>
+            </div>
+            <div class="sub-row1">
+                <div class="sub-row1-column1">
+                    <p>Planning your next trip?</p>
+                    <p>Subscribe toour newsletter. Get the latest travel trends & deals!6</p>
+                </div>
+                <div class="sub-row1-column2">
+                    <form action="">
+                        <div class="labelButton">
+                            <label for="">Enter Your Email</label>
+                            <button type="submit"><a href=""><img src="submit.png" alt=""></a></button>
+                        </div>
+                        <input type="email" name="email" id="email">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row2">
+            <div class="nav-menu">
+                <a href="">About Us</a>
+                <a href="">Mobile</a>
+                <a href="">Privacy</a>
+                <a href="">Terms of use</a>
+                <a href="">Career</a>
+                <a href="">Customer Service</a>
+            </div>
+            <div class="sosmed">
+                <a href=""><img src="fb.png" alt=""></a>
+                <a href=""><img src="ig.png" alt=""></a>
+                <a href=""><img src="twitter icon.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
